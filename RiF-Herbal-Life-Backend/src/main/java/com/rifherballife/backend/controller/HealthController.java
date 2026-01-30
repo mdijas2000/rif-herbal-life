62721d94e979c6667b376cf0df8cdeac21a3c0fb
mdijas2000 <mdijas2000@gmail.com>
2026-01-31 01:19:59 +0530

    Backend connect

---
+++ RiF-Herbal-Life-Backend/src/main/java/com/rifherballife/backend/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.rifherballife.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public String home(){
+        return "Backend is running";
+    }
+    @GetMapping("/api/health")
+    public String healthCheck(){
+        return "OK";
+    }
+}
