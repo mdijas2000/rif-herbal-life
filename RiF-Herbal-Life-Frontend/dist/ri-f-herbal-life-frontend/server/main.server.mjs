db2a5e570605a9761097a142ff66d2ec0f706b3e
mdijas2000 <mdijas2000@gmail.com>
2026-01-31 01:04:19 +0530

    Backend connect

--- RiF-Herbal-Life-Frontend/dist/ri-f-herbal-life-frontend/server/main.server.mjs
+++ RiF-Herbal-Life-Frontend/dist/ri-f-herbal-life-frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-AJX7SBK6.mjs";import"./chunk-Z6WML3NV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-CYMVURKW.mjs";import"./chunk-Z6WML3NV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
