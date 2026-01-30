0a1506522a05b4ada10fcad47a2c50f396848066
mdijas2000 <mdijas2000@gmail.com>
2026-01-31 01:15:20 +0530

    Backend connect

--- RiF-Herbal-Life-Frontend/dist/ri-f-herbal-life-frontend/server/main.server.mjs
+++ RiF-Herbal-Life-Frontend/dist/ri-f-herbal-life-frontend/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CYMVURKW.mjs";import"./chunk-Z6WML3NV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-2RS3VUUD.mjs";import"./chunk-Z6WML3NV.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- RiF-Herbal-Life-Frontend/src/environments/environment.prod.ts
+++ RiF-Herbal-Life-Frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     // START: TODO: Replace this with your actual backend URL when deployed
-    apiUrl: 'https://rif-herbal-life.onrender.com'
+    apiUrl: 'https://rif-herbal-life.onrender.com/api'
     // END: TODO
 };
