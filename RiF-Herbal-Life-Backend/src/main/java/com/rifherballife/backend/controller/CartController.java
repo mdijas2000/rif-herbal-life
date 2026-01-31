a5458a9dd75512822b18022dff0d057cc75981be
mdijas2000 <mdijas2000@gmail.com>
2026-01-31 11:44:12 +0530

    Registeration page fix

--- RiF-Herbal-Life-Backend/src/main/java/com/rifherballife/backend/controller/CartController.java
+++ RiF-Herbal-Life-Backend/src/main/java/com/rifherballife/backend/controller/CartController.java
@@ -3,7 +3,7 @@ package com.rifherballife.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rifherballife.backend.model.CartItem;
@@ -21,13 +20,7 @@ import org.springframework.security.core.Authentication;
 
 @RestController
 @RequestMapping("/api/cart")
-@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", methods = {
-		RequestMethod.GET,
-		RequestMethod.POST,
-		RequestMethod.PUT,
-		RequestMethod.DELETE,
-		RequestMethod.OPTIONS
-}, allowCredentials = "true")
+
 public class CartController {
 	@Autowired
 	private CartService cartService;
